41a6a2b1fe7704c9d3139cf691ead89af2631347
Elina Schäfer <elinaschaefer@googlemail.com>
2017-12-01 17:29:50 +0100

    first increment
    * text is now editable

--- lib/EditableTextComponent.jsx
+++ lib/EditableTextComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 
 const initialState = {
   text: '',
+  editing: false,
 };
 
 class EditableText extends React.Component {
@@ -13,31 +14,72 @@ class EditableText extends React.Component {
     this.state = initialState;
   }
 
+  startEditing() {
+    if (this.props.isTextEditable) {
+      this.setState({
+        editing: true,
+        text: this.props.text,
+      });
+    }
+  }
+
+  stopEditing() {
+    this.props.sendText(this.state.text);
+    this.setState({
+      editing: false,
+    });
+  }
+
+  renderText() {
+    if (!this.state.editing) {
+      return (
+        <TouchableOpacity onPress={() => this.startEditing()}>
+          <Text
+            style={this.props.textStyle}
+          >
+            {this.props.text}
+          </Text>
+        </TouchableOpacity>
+      );
+    }
+    return null;
+  }
+
+  renderTextInput() {
+    if (this.state.editing) {
+      return (
+        <TextInput
+          style={this.props.textInputStyle}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+          returnKeyType={'done'}
+          onSubmitEditing={() => this.stopEditing()}
+        />
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
       <View>
-        <Text
-          style={this.props.textStyle}
-        >
-          {this.props.text}
-        </Text>
-        <TextInput
-          style={this.props.textInputStyle}
-          onChangeText={() => this.setState()}
-          value={this.state.text}
-        />
+        {this.renderText()}
+        {this.renderTextInput()}
       </View>
     );
   }
 }
 
 EditableText.propTypes = {
+  isTextEditable: PropTypes.bool,
   text: PropTypes.string.isRequired,
+  sendText: PropTypes.func.isRequired,
   textStyle: PropTypes.shape(),
   textInputStyle: PropTypes.shape(),
 };
 
 EditableText.defaultProps = {
+  isTextEditable: true,
   textStyle: {},
   textInputStyle: {},
 };
